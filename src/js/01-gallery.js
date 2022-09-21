e105f86bbc1485ac7b6065788078c23b1760c8ec
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-09-21 21:26:59 +0300

    add-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const el = galleryItems.map(el => {
+  const image = document.createElement('img');
+  image.src = el.preview;
+  image.alt = el.description;
+  image.classList.add('gallery__image');
+
+  const item = document.createElement('a');
+  item.classList.add('gallery__item');
+  item.href = el.original;
+  item.dataset.alt = image.alt;
+
+  item.append(image);
+  return item;
+});
+
+gallery.append(...el);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+// import Player from '@vimeo/player';
 
+// const time = Player.querySelector('.focus-target');
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
